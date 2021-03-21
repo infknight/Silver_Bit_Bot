3c62ca3e985f046d41e36d6b99c0dd2970040b5d
Jason <jasonjia@tamu.edu>
2021-03-21 10:33:46 -0500

    added my default browser data to the application

--- bitbot.py
+++ bitbot.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
 
 password = input("Enter Password for FUT WebApp: \n")
+# adding to my default browser
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("user-data-dir=/Users/jasonjia/Library/ApplicationSupport/Google/Chrome")
+
 
 # start of the file
-driver = webdriver.Chrome(executable_path='/Users/jasonjia/Documents/personal_project/Silver_Bit_Bot/chromedriver')
+driver = webdriver.Chrome(executable_path='/Users/jasonjia/Documents/personal_project/Silver_Bit_Bot/chromedriver', chrome_options=options)
 driver.get("https://www.ea.com/fifa/ultimate-team/web-app/")
 # wait for EA to load
 time.sleep(10)
@@ -24,8 +31,17 @@ input_pd = driver.find_element_by_id("password")
 input_un.send_keys(user_name)
 input_pd.send_keys(password)
 
+# login button
+sec_login = driver.find_element_by_id("btnLogin")
+sec_login.click()
 
 
+# email verfication
+# do not need email verfication
+# email_send_code_btn = driver.find_element_by_id("btnSendCode")
+# email_send_code_btn.click()
+
+driver.close()
 
 
 
